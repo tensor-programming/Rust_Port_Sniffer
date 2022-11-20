7598d5826aa0b0d7b3eaf89b9f6f0c649ceefb29
Tensor <tensor@semantic-network.com>
2022-11-20 18:38:17 -0500

    update v1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,16 @@
 use std::env;
 use std::io::{self, Write};
 use std::net::{IpAddr, TcpStream};
-use std::str::FromStr;
 use std::process;
-use std::sync::mpsc::{Sender, channel};
+use std::str::FromStr;
+use std::sync::mpsc::{channel, Sender};
 use std::thread;
 
 const MAX: u16 = 65535;
 
 struct Arguments {
-    flag: String, 
-    ipaddr: IpAddr, 
+    flag: String,
+    ipaddr: IpAddr,
     threads: u16,
 }
 
@@ -23,30 +23,39 @@ impl Arguments {
         }
         let f = args[1].clone();
         if let Ok(ipaddr) = IpAddr::from_str(&f) {
-            return Ok(Arguments {flag: String::from(""), ipaddr, threads: 4});
+            return Ok(Arguments {
+                flag: String::from(""),
+                ipaddr,
+                threads: 4,
+            });
         } else {
             let flag = args[1].clone();
             if flag.contains("-h") || flag.contains("-help") && args.len() == 2 {
-                println!("Usage: -j to select how many threads you want
-                \n\r       -h or -help to show this help message");
+                println!(
+                    "Usage: -j to select how many threads you want
+                \n\r       -h or -help to show this help message"
+                );
                 return Err("help");
             } else if flag.contains("-h") || flag.contains("-help") {
                 return Err("too many arguments");
             } else if flag.contains("-j") {
                 let ipaddr = match IpAddr::from_str(&args[3]) {
                     Ok(s) => s,
-                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6")
+                    Err(_) => return Err("not a valid IPADDR; must be IPv4 or IPv6"),
                 };
-                let threads = match args[2].parse::<u16>(){
+                let threads = match args[2].parse::<u16>() {
                     Ok(s) => s,
-                    Err(_) => return Err("failed to parse thread number")
+                    Err(_) => return Err("failed to parse thread number"),
                 };
-                return Ok(Arguments{threads, flag, ipaddr});
+                return Ok(Arguments {
+                    threads,
+                    flag,
+                    ipaddr,
+                });
             } else {
                 return Err("invalid syntax");
             }
-        } 
-        
+        }
     }
 }
 
@@ -59,8 +68,7 @@ fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
                 io::stdout().flush().unwrap();
                 tx.send(port).unwrap();
             }
-            Err(_) => {}    
-            
+            Err(_) => {}
         }
 
         if (MAX - port) <= num_threads {
@@ -73,16 +81,14 @@ fn scan(tx: Sender<u16>, start_port: u16, addr: IpAddr, num_threads: u16) {
 fn main() {
     let args: Vec<String> = env::args().collect();
     let program = args[0].clone();
-    let arguments = Arguments::new(&args).unwrap_or_else(
-        |err| {
-            if err.contains("help") {
-                process::exit(0);
-            } else {
-                eprintln!("{} problem parsing arguments: {}", program, err);
-                process::exit(0);
-            }
+    let arguments = Arguments::new(&args).unwrap_or_else(|err| {
+        if err.contains("help") {
+            process::exit(0);
+        } else {
+            eprintln!("{} problem parsing arguments: {}", program, err);
+            process::exit(0);
         }
-    );
+    });
 
     let num_threads = arguments.threads;
     let addr = arguments.ipaddr;
@@ -99,7 +105,7 @@ fn main() {
     drop(tx);
     for p in rx {
         out.push(p);
-    }    
+    }
 
     println!("");
     out.sort();
@@ -107,4 +113,3 @@ fn main() {
         println!("{} is open", v);
     }
 }
-
